bcde878ecd0bf65e2dacc83b82e119992a7d9ee2
DanielE <danenrdia@alum.us.es>
2022-08-20 13:13:28 +0200

    Task-12: Done
    Añadida entidad "Peep"

---
+++ src/main/java/acme/entities/Peep.java
@@ -0,0 +1,54 @@
+package acme.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Peep extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+	
+	private static final long serialVersionUID = 1L;
+		
+	// Attributes -------------------------------------------------------------
+	
+	@NotNull
+	@Past
+	@Temporal(value = TemporalType.TIMESTAMP)
+	protected Date instantiationMoment;
+	
+	@NotBlank
+	@Length(max = 100)
+	protected String heading;
+	
+	@NotBlank
+	@Length(max = 100)
+	protected String writer;
+	
+	@NotBlank
+	@Length(max = 255)
+	protected String text;
+	
+	@Email
+	protected String optionalEmail;
+	
+	// Derived attributes -----------------------------------------------------
+	
+	// Relationships ----------------------------------------------------------
+	
+}
