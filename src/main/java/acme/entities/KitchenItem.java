1644ab54f4e8942e630b4e7d47fc55ebeb453670
Andres Vargas <andvarbay@alum.us.es>
2022-08-20 18:15:33 +0200

    Task-14 & Task-15: Done
    Creación de la entidad KitchenItem, del enumerado KitchenItemType y de
    la entidad Quantity para relacionar KitchenItem con Recipe.

---
+++ src/main/java/acme/entities/KitchenItem.java
@@ -0,0 +1,68 @@
+
+package acme.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.datatypes.Money;
+import acme.framework.entities.AbstractEntity;
+import acme.roles.Chef;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class KitchenItem extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotNull
+	protected KitchenItemType type;
+
+	@NotBlank
+	@Length(max = 100)
+	protected String name;
+
+	@NotBlank
+	@Pattern(regexp = "^([A-Z]{2}:)?[A-Z]{3}-[0-9]{3}$", message = "wrong pattern, example -> AB:019-A")
+	@Column(unique = true)
+	protected String code;
+
+	@NotBlank
+	@Length(max = 255)
+	protected String description;
+
+	@NotNull
+	@Valid
+	protected Money retailPrice;
+
+	@NotNull
+	protected boolean published;
+
+	@URL
+	protected String optionalLink;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Chef chef;
+
+}
---
+++ src/main/java/acme/entities/KitchenItemType.java
@@ -0,0 +1,6 @@
+package acme.entities;
+
+
+public enum KitchenItemType {
+	INGREDIENT, KITCHEN_UTENSIL;
+}
