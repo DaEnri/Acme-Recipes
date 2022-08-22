0a7fe73611fbfdf4ffd7fad535c38bf4d2f41c2a
DanielE <danenrdia@alum.us.es>
2022-08-22 19:02:13 +0200

    Task-20: Done
    Añadida entidad "AdminDashboard"

---
+++ src/main/java/acme/forms/AdminDashboard.java
@@ -0,0 +1,46 @@
+package acme.forms;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import acme.entities.Status;
+import acme.framework.datatypes.Money;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminDashboard implements Serializable{
+	
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long			serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	
+	int									totalNumberIngredients;
+	
+	Map<String, Money>					averageRetailPriceIngredientsByCurrency;
+	Map<String, Money>					deviationRetailPriceIngredientsByCurrency;
+	Map<String, Money>					minimumRetailPriceIngredientsByCurrency;
+	Map<String, Money>					maximumRetailPriceIngredientsByCurrency;
+	
+	int									totalNumberKitchenUtensils;
+	
+	Map<String, Money>					averageRetailPriceKitchenUtensilsByCurrency;
+	Map<String, Money>					deviationRetailPriceKitchenUtensilsByCurrency;
+	Map<String, Money>					minimumRetailPriceKitchenUtensilsByCurrency;
+	Map<String, Money>					maximumRetailPriceKitchenUtensilsByCurrency;
+	
+	Map<Status, Integer>				totalNumberFineDishesOfStatus;
+	
+	Money								averageBudgetFineDishesOfStatus;
+	Money								deviationBudgetFineDishesOfStatus;
+	Money								minimumBudgetFineDishesOfStatus;
+	Money								maximumBudgetFineDishesOfStatus;
+	
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+	
+}
