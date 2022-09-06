06ca469973d10fe34e46db6409f1bc4847d64405
DanielE <danenrdia@alum.us.es>
2022-09-07 00:19:47 +0200

    Task-63: Done
    Se ha añadido en los forms que presentaban presupuestos/precios el
    cambio a la moneda del sistema

--- src/main/java/acme/features/chef/fineDish/ChefFineDishRepository.java
+++ src/main/java/acme/features/chef/fineDish/ChefFineDishRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import acme.entities.FineDish;
 import acme.framework.repositories.AbstractRepository;
+import acme.system.configuration.SystemConfiguration;
 
 @Repository
 public interface ChefFineDishRepository extends AbstractRepository {
@@ -19,5 +20,8 @@ public interface ChefFineDishRepository extends AbstractRepository {
 	
 	@Query("select f from FineDish f where f.chef.id = :id")
 	Collection<FineDish> findFineDishesByChefId(int id);
+	
+	@Query("select i from SystemConfiguration i")
+	SystemConfiguration getSystemConfiguration();
 
 }
